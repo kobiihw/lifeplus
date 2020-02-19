e5618595042d5cccf87c691ce7c63004d53a4ebf
kobiihw <54731242+kobiihw@users.noreply.github.com>
2020-02-18 20:52:37 -0600

    trying to connect to jawsdb

--- models/index.js
+++ models/index.js
@@ -9,12 +9,22 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL); 
+ } else {
+ connection = mysql.createConnection({
+     host: "localhost",
+     port: 3306,
+     user: "root",
+     password: "Password1",
+     database: "lifeplus_db"
+   })
+ };
 fs
   .readdirSync(__dirname)
   .filter(file => {
